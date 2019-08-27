87805aafa447931620483aebfb88ac26fd8734ab
LeoRoma <xiajtn@gmail.com>
2019-08-27 21:25:52 +0100

    is able to print a statement. IMPORTANT I just realized a bug in BankStatement class, the variable @current_balance needs to be modified

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,10 +1,16 @@
 class BankStatement
 attr_reader :transactions
   def initialize
-    @transactions = []
+    @transactions = ["date || credit || debit || balance"]
   end
 
   def add_transaction(transaction)
      @transactions << "#{transaction.description}"
+  end
+
+  def print
+    @transactions.each do |transaction|
+      transaction
+    end
   end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -2,16 +2,31 @@ require 'bank_statement'
 
 describe BankStatement do
 let(:statement) { BankStatement.new }
-let(:transaction) { BankTransaction.new }
+let(:transaction1) { BankTransaction.new }
+let(:transaction2) { BankTransaction.new }
 
-
-  describe '#add_transiction' do
+  describe '#add_transaction' do
     it "updates the transaction of the deposit into a list" do
-      allow(transaction).to receive(:description) { "10/01/2012 || 1000 || || 1000" }
-      statement.add_transaction(transaction)
+      allow(transaction1).to receive(:description) { "10/01/2012 || 1000 || || 1000" }
+      statement.add_transaction(transaction1)
       expect(statement.transactions).to include "10/01/2012 || 1000 || || 1000"
     end
 
+    it "updates the transaction of the withdrawal into a list" do
+      allow(transaction2).to receive(:description) { "14/01/2012 || || 500 || 2500" }
+      statement.add_transaction(transaction2)
+      expect(statement.transactions).to include "14/01/2012 || || 500 || 2500"
+    end
+  end
+
+  describe '#print' do
+    it "prints the statement" do
+      allow(transaction1).to receive(:description) { "10/01/2012 || 1000 || || 1000" }
+      statement.add_transaction(transaction1)
+      allow(transaction2).to receive(:description) { "14/01/2012 || || 500 || 2500" }
+      statement.add_transaction(transaction2)
+      expect(statement.print).to eq ["date || credit || debit || balance", "10/01/2012 || 1000 || || 1000", "14/01/2012 || || 500 || 2500"]
+    end
   end
 
 end

959a74fe124603ee2c64172303755a4fc3b2dc42
LeoRoma <xiajtn@gmail.com>
2019-08-27 20:25:15 +0100

    added Statement class

---
+++ lib/bank_statement.rb
@@ -0,0 +1,10 @@
+class BankStatement
+
+  def initialize
+    @transictions = []
+  end
+
+  def add_transiction(transiction)
+    
+  end
+end
---
+++ spec/bank_statement_spec.rb
@@ -0,0 +1,12 @@
+require 'bank_statement'
+
+describe BankStatement do
+let(:statement) { BankStatement.new }
+
+  describe '#add_transiction' do
+    it "adds a transiction into a list" do
+
+    end
+  end
+
+end

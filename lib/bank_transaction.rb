e0580c1dfe3d224b63bb16e68e1a716bd2c15773
LeoRoma <xiajtn@gmail.com>
2019-08-27 20:20:05 +0100

    a customer can withdraw money

--- lib/bank_transaction.rb
+++ lib/bank_transaction.rb
@@ -1,12 +1,20 @@
 class BankTransaction
-attr_reader :current_balance
+attr_reader :current_balance, :description
 
   def initialize
     @current_balance = 0
+    @description = ""
   end
 
   def deposit(amount)
+    # p amount_float = '%.2f' % amount
     @current_balance += amount
-  end   
+    @description << "10/01/2012 || #{amount} || || #{current_balance}"
+  end
+
+  def withdrawal(amount)
+    @current_balance -= amount
+    @description << "14/01/2012 || || #{amount} || #{current_balance}"
+  end
 
 end
--- spec/bank_transaction_spec.rb
+++ spec/bank_transaction_spec.rb
@@ -8,9 +8,28 @@ let(:transaction) { BankTransaction.new }
   end
 
   describe "#deposit" do
-    it 'gives the possibility to deposit a certain amount' do
+    it 'gives the possibility to deposit a certain amount of money' do
       transaction.deposit(100)
       expect(transaction.current_balance).to eq 100
+    end
+
+    it 'add the description of the completed transaction' do
+      transaction.deposit(1000)
+      expect(transaction.description).to eq "10/01/2012 || 1000 || || 1000"
+    end
+  end
+
+  describe "#withdrawal" do
+    it 'gives the possibility to withdraw a certain amount of money' do
+      transaction.deposit(100)
+      transaction.withdrawal(50)
+      expect(transaction.current_balance).to eq 50
+    end
+
+    it 'add the description of the completed transaction' do
+      transaction.deposit(3000)
+      transaction.withdrawal(500)
+      expect(transaction.description).to include "14/01/2012 || || 500 || 2500"
     end
   end
 
